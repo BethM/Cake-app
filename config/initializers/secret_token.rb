75af70b41c32f53d6b285b4ca2903e4a4193fbdd
BethM <medina.beth@gmail.com>
2013-06-23 17:01:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cake::Application.config.secret_token = 'de18588d27efbbce054186609e834513f8b9b845af8feda2e1c34695f00ab155551df28d875e9b6cb8aea97f841c930e1be8b5ae7847d6ac88413c7e79758bbd'
